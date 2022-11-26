93284709e8c40afdd2be632eff0ec4f09ba143aa
Ralph Depping <github@depping.me>
2022-11-26 18:38:04 +0000

    individual services to subgraphs

--- subgraph-locations/index.js
+++ subgraph-locations/index.js
@@ -7,9 +7,11 @@ const gql = require('graphql-tag');
 const typeDefs = gql(readFileSync('./locations.graphql', { encoding: 'utf-8' }));
 const resolvers = require('./resolvers');
 const LocationsAPI = require('./datasources/LocationsApi');
+const {buildSubgraphSchema} = require("@apollo/subgraph");
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer( {schema: buildSubgraphSchema({typeDefs, resolvers}) });
+
 
   const port = 4001;
   const subgraphName = 'locations';
